f6febf43fb2ff78fe043def4ac6184e1b708b594
Papp, Tamás <tpapp@itron.com>
2018-12-20 02:02:07 +0100

    add: docker pull

--- init-cosmosdb-emulator.ps1
+++ init-cosmosdb-emulator.ps1
@@ -6,4 +6,6 @@ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path
 . "$PSScriptRoot/common.ps1"
 
 New-Item -ItemType Directory -Force -Path $hostDirectory | Out-Null
+
+docker pull microsoft/azure-cosmosdb-emulator
 docker run --name $containerName -p 8081:8081 -h azure-cosmosdb-emulator --memory 2GB --mount "type=bind,source=$hostDirectory,destination=C:\CosmosDB.Emulator\bind-mount" -P --interactive --tty microsoft/azure-cosmosdb-emulator
